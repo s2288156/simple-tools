4cf4fe213197dddca58bbb4f83363987a0f49616
JackWu <s2288156@163.com>
2023-11-23 16:41:57 +0800

    feat: clean code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheHello from '@/components/TheHello.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <TheHello />
+  </div>
 </template>
